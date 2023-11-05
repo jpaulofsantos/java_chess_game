2cff199bed2c79640d920e02eea9cd8e71121a10
Joao Paulo <jpaulofsantos@gmail.com>
2023-11-05 20:01:34 -0300

    bishop piece

--- src/chess/pieces/Bishop.java
+++ src/chess/pieces/Bishop.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -15,6 +16,49 @@ public class Bishop extends ChessPiece {
     }
     @Override
     public boolean[][] possibleMoves() {
-        return new boolean[getBoard().getRows()][getBoard().getColumns()];
+        boolean[][] matriz = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+        Position p = new Position(0,0 );
+
+        //checking positions nw
+        p.setValues(position.getRow()-1, position.getColumn()-1);
+        while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) { //percorre as linhas verificando se a posição existe e se existe uma peça
+            matriz[p.getRow()][p.getColumn()] = true;
+            p.setValues(p.getRow()-1, p.getColumn()-1);
+        }
+        if (getBoard().positionExists(p) && isThereOpponentPiece(p)) { //verifica se a posição existe e se existe uma peça do adversário
+            matriz[p.getRow()][p.getColumn()] = true;
+        }
+
+        //checking positions ne
+        p.setValues(position.getRow() -1, position.getColumn()+1);
+        while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) { //percorre as linhas verificando se a posição existe e se existe uma peça
+            matriz[p.getRow()][p.getColumn()] = true;
+            p.setValues(p.getRow()-1, p.getColumn()+1);
+        }
+        if (getBoard().positionExists(p) && isThereOpponentPiece(p)) { //verifica se a posição existe e se existe uma peça do adversário
+            matriz[p.getRow()][p.getColumn()] = true;
+        }
+
+        //checking positions se
+        p.setValues(position.getRow()+1, position.getColumn()+1);
+        while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) { //percorre as linhas verificando se a posição existe e se existe uma peça
+            matriz[p.getRow()][p.getColumn()] = true;
+            p.setValues(p.getRow()+1, p.getColumn()+1);
+        }
+        if (getBoard().positionExists(p) && isThereOpponentPiece(p)) { //verifica se a posição existe e se existe uma peça do adversário
+            matriz[p.getRow()][p.getColumn()] = true;
+        }
+
+        //checking positions sw
+        p.setValues(position.getRow()+1, position.getColumn()-1);
+        while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) { //percorre as linhas verificando se a posição existe e se existe uma peça
+            matriz[p.getRow()][p.getColumn()] = true;
+            p.setValues(p.getRow()+1, p.getColumn()-1);
+        }
+        if (getBoard().positionExists(p) && isThereOpponentPiece(p)) { //verifica se a posição existe e se existe uma peça do adversário
+            matriz[p.getRow()][p.getColumn()] = true;
+        }
+        return matriz;
     }
 }
